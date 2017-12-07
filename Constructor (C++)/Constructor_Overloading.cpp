2bfb8d1470fc9681bc7d2b94808cc5e8d32be97c
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2017-12-07 19:57:13 +0530

    Commit

---
+++ Constructor (C++)/Constructor_Overloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Construct()
+{
+	public:
+		float area;
+		Construct() { area = 0; }
+		Construct(int a ,int b)
+		{
+			area = a*b ;
+		}
+		void disp()
+		{
+			cout << area << endl;
+		}
+};
+
+int main()
+{
+	Construct o1;
+	Construct o2(10,20);
+	o1.disp();
+	o2.disp();
+	return 1;
+}
+
