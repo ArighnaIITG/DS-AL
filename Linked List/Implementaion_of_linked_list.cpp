696dbfae4cc53bf0851017ad4e5d0be344ec1849
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2017-12-22 11:32:51 +0530

    Added Linked List Changes

--- Linked List/Implementaion_of_linked_list.cpp
+++ Linked List/Implementaion_of_linked_list.cpp
@@ -16,7 +16,24 @@ struct Node
 	struct Node *next;   // Pointer to the next node
 };
 
+struct Node* head;       // Head Node of the Linked List
+
 class LinkedList()
 {
-	
+	public :
+		Node* create Node(int value);       // Creating a node with a given value
+		void insert_begin();				// Inserting a node at the beginning of the linked list
+        void insert_pos();					// Inserting a node at a specific position in the linked list
+        void insert_last(); 				// Inserting a node at the end of the linked list
+        void delete_pos();					// Deleting a node at a specific position in the linked list
+        void sort();
+        void search();
+        void update();
+        void reverse();
+        void display();
+
+        LinkedList() 
+        {
+            head = NULL;
+        }
 };
