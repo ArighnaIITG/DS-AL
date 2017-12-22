8aa236d7067d37e12546ddaf2e629ccef6988e2b
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2017-12-22 11:21:24 +0530

    Added Linked List Changes

---
+++ Linked List/Implementaion_of_linked_list.cpp
@@ -0,0 +1,22 @@
+/* This program will implement the various operations possible in a linked list,
+   like inserting a node at various positions, or deleting a node, or reversing 
+   the linked list, etc
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+// A Linked List Node
+struct Node
+{
+	int data;  // Data fields of the nodes
+	struct Node *next;   // Pointer to the next node
+};
+
+class LinkedList()
+{
+	
+};
