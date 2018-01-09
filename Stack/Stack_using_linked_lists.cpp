8a9eb34d9c425c0e4fa4a4ae4070914318cc4cce
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-01-09 12:21:43 +0530

    Updated Stack Using Linked Lists

--- Stack/Stack_using_linked_lists.cpp
+++ Stack/Stack_using_linked_lists.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
+#include <climits>
 
 using namespace std;
 
@@ -15,16 +16,16 @@ struct StackNode
 {
 	int data;
 	struct StackNode* next;
-}
+};
 
-class Stack_LL()
+class Stack_LL
 {
 	public :
 		StackNode* new_Node(int data);
 		int isEmpty(StackNode *head);
 		void push(StackNode **head, int data);
 		int pop(StackNode **head);
-		int peek(StackNode **head);
+		int peek(StackNode *head);
 };
 
 StackNode* Stack_LL::new_Node(int data)
@@ -37,14 +38,49 @@ StackNode* Stack_LL::new_Node(int data)
 
 int Stack_LL::isEmpty(StackNode *head)
 {
-	return !head;
+	return head == NULL;
 }
 
-void Stack_LL::push(StackNode **head, int data)
+void Stack_LL::push(StackNode** head, int data)
 {
 	StackNode *new_node = new_Node(data);
 	new_node->next = *head;
 	*head = new_node;
-	cout << data << " is pushed to the stack." << endl 
+	cout << data << " is pushed to the stack." << endl;
 }
 
+int Stack_LL::pop(StackNode** head)
+{
+	if (isEmpty(*head))
+	{
+		return INT_MIN;
+	}
+	StackNode *temp = *head;
+	*head = (*head)->next;
+	int popped = temp->data;
+	free(temp);
+	return popped;
+}
+
+int Stack_LL::peek(StackNode *head)
+{
+	if (isEmpty(head))
+	{
+		return INT_MIN;
+	}
+	return head->data;
+}
+
+int main()
+{
+	StackNode *head = NULL;
+	Stack_LL stack;
+	stack.push(&head, 10);
+	stack.push(&head, 20);
+	stack.push(&head, 30);
+	cout << stack.pop(&head) << " popped from stack" << endl;
+ 
+    cout << "Top element is " << stack.peek(head) << endl;
+ 
+    return 0;
+}
