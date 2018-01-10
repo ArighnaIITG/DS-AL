3dbecbe51c23bdfcfd500c1278acc1cfb64ae625
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-01-11 03:49:58 +0530

    Added queus

---
+++ Queue/Queue_Implementation.cpp
@@ -0,0 +1,28 @@
+/* This program implements the data structures called queues. Queue follows the FIFO structure, 
+   i.e. , the first item to enter the queue is the first item to be popped out of the queue.
+
+   Mainly the following four basic operations are performed on queue:
+
+        Enqueue: Adds an item to the queue. If the queue is full, then it is said to be 
+        		 an Overflow condition.
+
+		Dequeue: Removes an item from the queue. The items are popped in the same order in 
+				 which they are pushed. If the queue is empty, then it is said to be an 
+				 Underflow condition.
+
+		Front: Get the front item from queue.
+		Rear: Get the last item from queue.
+
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+class Queue
+{
+	public :
+		
+}
