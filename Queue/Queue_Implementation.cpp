1e89de4a216978467a77f82b2a26dd9ea9afb001
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-01-15 15:15:54 +0530

    Modified Queue Implementation

--- Queue/Queue_Implementation.cpp
+++ Queue/Queue_Implementation.cpp
@@ -28,12 +28,23 @@ struct Queue
 	int front, rear, size;
 	unsigned capacity;
 	int* array;
-}
+};
 
 class QueueImp
 {
 	public :
-		Queue
+		Queue* createQueue(unsigned capacity);
+		int isFull(Queue* queue);
+		int isEmpty(Queue* queue);
+		void enqueue(Queue* queue, int item);
+		int dequeue(Queue* queue);
+		int front(Queue* queue);
+		int rear(Queue* queue);
+};
 
-
+Queue* QueueImp::createQueue(unsigned capacity)
+{
+	Queue *queue = new Queue;
+	queue->front = queue->size =0;
+	
 }
