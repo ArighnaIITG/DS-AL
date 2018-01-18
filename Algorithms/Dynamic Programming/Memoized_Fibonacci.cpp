7b46b58b649a91116a6df1bc56aa635d01001b85
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-01-19 03:01:25 +0530

    Added memoization

--- Algorithms/Dynamic Programming/Memoized_Fibonacci.cpp
+++ Algorithms/Dynamic Programming/Memoized_Fibonacci.cpp
@@ -32,5 +32,18 @@ void initialize()
 
 int fib(int n)
 {
+	if(lookup[n] == NIL)
+	{
+		if (n <= 1)
+			lookup[n] = n;
+
+		else
+			lookup[n] = fib(n-1) + fib(n-2);
+	}
+	return lookup[n];
+}
+
+int main()
+{
 	
 }
