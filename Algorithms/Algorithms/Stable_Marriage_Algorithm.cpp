0dd8f7eb055fb3d5507622f378f5f84760c1af2f
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-01-22 03:30:23 +0530

    Updated Stable Marriage Problem

--- Algorithms/Algorithms/Stable_Marriage_Algorithm.cpp
+++ Algorithms/Algorithms/Stable_Marriage_Algorithm.cpp
@@ -67,7 +67,7 @@ using namespace std;
 #define N 4
 
 // Returns true if woman 'w' prefers a man 'm1' over another man 'm'.
-bool man_preference(int prefer[2*N][N], int w, int m1, int m)
+bool man_preference(int prefer[2*N][N], int w, int m, int m1)
 {
 	for(int i=0; i<N; i++)
 	{
@@ -83,3 +83,81 @@ bool man_preference(int prefer[2*N][N], int w, int m1, int m)
 	}
 }
 
+// Prints stable matching for N boys and N girls. Boys are numbered as 0 to
+// N-1. Girls are numbereed as N to 2N-1.
+void stable_marriage_problem(int prefer[2*N][N])
+{
+	// Stores partner of women. This is our output array that
+    // stores paing information.  The value of wPartner[i]
+    // indicates the partner assigned to woman N+i.  Note that
+    // the woman numbers between N and 2*N-1. The value -1
+    // indicates that (N+i)'th woman is free
+    int wPartner[N];
+ 
+    // An array to store availability of men.  If mFree[i] is
+    // false, then man 'i' is free, otherwise engaged.
+    bool mFree[N];
+
+    // Initialize all men and women as free
+    memset(wPartner, -1, sizeof(wPartner));
+    memset(mFree, false, sizeof(mFree));
+    int freeCount = N;
+
+    // While there are free men
+    while(freeCount > 0)
+    {
+    	// Pick the first free man
+    	int m;
+    	for(m=0; m<N; m++)
+    	{
+    		if(mFree[m] == false)
+    			break;
+    	}
+
+    	for(int i=0; i<N && mFree[m] == false; i++)
+    	{
+    		int w = prefer[m][i];
+
+    		if(wPartner[w-N] == -1)
+    		{
+    			wPartner[w-N] = m;
+    			mFree[m] = true;
+    			freeCount--;
+    		}
+
+    		else
+    		{
+    			int m1 = wPartner[w-N];
+
+    			if(man_preference(prefer, w, m, m1) == false)
+    			{
+    				wPartner[w-N] = m;
+    				mFree[m] = true;
+    				mFree[m1] = false;
+    			}
+    		}
+    	}
+    }
+
+    // Print the solution
+    cout << "Woman   Man" << endl;
+    for (int i = 0; i < N; i++)
+       cout << " " << i+N << "\t\t" << wPartner[i] << endl;
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int prefer[2*N][N] = { {7, 5, 6, 4},
+        {5, 4, 6, 7},
+        {4, 5, 6, 7},
+        {4, 5, 6, 7},
+        {0, 1, 2, 3},
+        {0, 1, 2, 3},
+        {0, 1, 2, 3},
+        {0, 1, 2, 3},
+    };
+    stable_marriage_problem(prefer);
+ 
+    return 0;
+}
