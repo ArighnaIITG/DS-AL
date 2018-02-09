ea7ed096621dfbfb999b910da2a2ceac5462ed7e
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-02-10 01:35:32 +0530

    Updated files

--- Data Structures/Trees/Various_depth_first_traversals_in_trees.cpp
+++ Data Structures/Trees/Various_depth_first_traversals_in_trees.cpp
@@ -1,7 +1,32 @@
-// Program for implementing the various tree traversals.
+/*
+  
+   Program for implementing the various tree traversals.
+
+   The various tree traversals are :
+
+   1. Inorder - Left->Root->Right
+   2. Preorder - Root->Left->Right
+   3. Postorder - Left->Right->Root
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 using namespace std;
+
+// Represents a tree node
+struct Node
+{
+	int data;
+	struct Node* left;
+	struct Node* right;	
+};
+
+class TreeTraversal
+{
+	public:
+		
+
+}
 
