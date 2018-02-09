00536e2e2f457ecb5059c3548d3fa38cfbd23530
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-02-10 04:59:18 +0530

    Updated files

---
+++ Data Structures/Trees/BinarySearchTree.cpp
@@ -0,0 +1,39 @@
+/* 
+	
+	BINARY SEARCH TREES (BST) :
+	=========================
+
+	Binary Search Tree, is a node-based binary tree data structure which has the following properties:
+
+    	1. The left subtree of a node contains only nodes with keys less than the node’s key.
+    	2. The right subtree of a node contains only nodes with keys greater than the node’s key.
+    	3. The left and right subtree each must also be a binary search tree.
+    	4. There must be no duplicate nodes.
+	
+	This program will implement how to search a given key and also insert a given node in a BST.
+	Done by the recuresive strategy.
+
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+	int data;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+class BinarySearchTree
+{
+	public:
+		TreeNode* newBSTNode(int data);
+		void inorder(TreeNode* node);
+		TreeNode* insertBSTNode(TreeNode* node, int key);
+		TreeNode* searchBSTNode(TreeNode* node, int key);
+}
+
