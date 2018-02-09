748d93497925133b2e7bee589d500d47d11ccd34
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-02-10 01:46:10 +0530

    Updated files

--- Data Structures/Trees/Various_depth_first_traversals_in_trees.cpp
+++ Data Structures/Trees/Various_depth_first_traversals_in_trees.cpp
@@ -8,6 +8,8 @@
    2. Preorder - Root->Left->Right
    3. Postorder - Left->Right->Root
 
+   It will be implemented using a recursive strategy.
+
 */
 
 #include <stdio.h>
@@ -32,5 +34,45 @@ class TreeTraversal
 		void printPreorder(TreeNode* node);
 };
 
+TreeNode* TreeTraversal::newTreeNode(int data)
+{
+	TreeNode *newnode = new TreeNode;
+	newnode->data = data;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return newnode;
+}
 
+void printInorder(TreeNode* node)
+{
+	if(node == NULL)
+		return;
+
+	// Implemented the recurring strategy
+	printInorder(node->left);
+	cout << node->data << " - " ;
+	printInorder(node->right);
+}
+
+void printPostorder(TreeNode* node)
+{
+	if(node == NULL)
+		return;
+
+	// Implemented the recurring strategy
+	printInorder(node->left);
+	printInorder(node->right);
+	cout << node->data << " - " ;
+}
+
+void printPreorder(TreeNode* node)
+{
+	if(node == NULL)
+		return;
+
+	// Implemented the recurring strategy
+	cout << node->data << " - " ;
+	printInorder(node->left);
+	printInorder(node->right);
+}
 
