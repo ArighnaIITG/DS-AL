7146b1abe2de776a90cc98908c1c61b8102728ff
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-05-10 08:21:45 +0530

    Added new greedy algorithms

--- Algorithms/Greedy Algorithms/Djikstra's_Algorithm.cpp
+++ Algorithms/Greedy Algorithms/Djikstra's_Algorithm.cpp
@@ -25,17 +25,17 @@ int min_distance(int dist[], bool sptSet[])
 
 void printSolution(int dist[], int n)
 {
-	printf("Vertex   Distance from Source\n");
+	printf("Vertex Distance from Source\n");
 
     for (int i = 0; i < V; i++)
-    	printf("%d tt %d\n", i, dist[i]);
+    	printf("%d\n", dist[i]);
 }
 
 // Implements the Djikstra's algorithm for shortest path in a graph,
 // represented by adjacency matrix.
 void djikstra(int graph[V][V], int src)
 {
-	int dist [V];   // The output array. dist[i] will hold the shortest
+	int dist[V];   // The output array. dist[i] will hold the shortest
                     // distance from src to i
 	bool sptSet[V]; // A boolean array, stating if vertex V is in the SPT.
  
@@ -48,7 +48,7 @@ void djikstra(int graph[V][V], int src)
 
 	dist[src] = 0;
 
-	for(int count=0; count<V-1; count++)
+	for(int count=0; count<(V-1); count++)
 	{
 		int u = min_distance(dist, sptSet);
 		sptSet[u] = true;
@@ -76,7 +76,7 @@ int main()
                       {0, 0, 2, 0, 0, 0, 6, 7, 0}
                       };
   
-    dijkstra(graph, 0);
+    djikstra(graph, 0);
   
     return 0;
 }
